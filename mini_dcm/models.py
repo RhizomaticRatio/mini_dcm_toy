89b9f465a963916e3fe2de61d092a7ca54ce5d5e
Oğulcan Ulu <ogulcanulu@Ogulcans-MacBook-Air-3.local>
2025-11-25 21:52:44 +0100

    Competing models updated

--- mini_dcm/models.py
+++ mini_dcm/models.py
@@ -15,3 +15,42 @@ def effective_connectivity():
 
     return A, C
 
+
+#hypothesis to tests
+
+def model_mask_chain_true(): #the model aligns with the true connectivity
+    """
+    Model 1: correct chain 1 -> 2 -> 3, with self-connections on all nodes.
+    """
+    mask = np.zeros((3, 3), dtype=bool) #the sturcture of the A matrix
+
+    np.fill_diagonal(mask, True) #self-connections
+
+    mask[1, 0] = True   # node 1 -> node 2
+    mask[2, 1] = True   #node 2 -> node 3
+    return mask
+
+
+def model_mask_missing_2to3():
+    """
+    Model 2: same as chain, but without 2 -> 3 connection.
+    """
+    mask = np.zeros((3, 3), dtype=bool)
+
+    np.fill_diagonal(mask, True)
+    mask[1, 0] = True   # 1 -> 2
+    return mask
+
+
+def model_mask_wrong_3to1():
+    """
+    Model 3: chain plus wrong feedback 3 -> 1 instead of 2 -> 3.
+    """
+    mask = np.zeros((3, 3), dtype=bool)
+
+    np.fill_diagonal(mask, True)
+    mask[1, 0] = True   # 1 -> 2
+    mask[0, 2] = True   # 3 -> 1
+    # 2 -> 3 absent
+
+    return mask
